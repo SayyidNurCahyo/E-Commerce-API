be6c7ec4af7a430ae0f1ba8f73cd654764f4e171
Sayyid Nur Cahyo Abdul Jalil <sayyidnurc123@gmail.com>
2024-02-28 18:36:28 +0700

    [FEATURE] add constant package, make ConstantTable to store table name and APIUrl to store URI for every Controller

---
+++ src/main/java/com/enigma/wmbapi/constant/APIUrl.java
@@ -0,0 +1,9 @@
+package com.enigma.wmbapi.constant;
+
+public class APIUrl {
+    public static final String CUSTOMER_API = "/api/customers";
+    public static final String TRANS_TYPE_API = "/api/transTypes";
+    public static final String TABLE_API = "/api/tables";
+    public static final String MENU_API = "/api/menus";
+    public static final String TRANSACTION_API = "/api/transactions";
+}
