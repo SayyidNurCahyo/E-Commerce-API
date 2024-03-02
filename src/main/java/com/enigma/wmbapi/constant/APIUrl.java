eed34bb3611d96c290bea915ecae19b0293b18ab
Sayyid Nur Cahyo Abdul Jalil <sayyidnurc123@gmail.com>
2024-03-02 10:00:50 +0700

    [FEATURE] add configuration for bean and user role to get authorities

--- src/main/java/com/enigma/wmbapi/constant/APIUrl.java
+++ src/main/java/com/enigma/wmbapi/constant/APIUrl.java
@@ -1,7 +1,7 @@
 package com.enigma.wmbapi.constant;
 
 public class APIUrl {
-    public static final String CUSTOMER_API = "/api/customers";qq
+    public static final String CUSTOMER_API = "/api/customers";
     public static final String TRANS_TYPE_API = "/api/transTypes";
     public static final String TABLE_API = "/api/tables";
     public static final String MENU_API = "/api/menus";
---
+++ src/main/java/com/enigma/wmbapi/constant/UserRole.java
@@ -0,0 +1,5 @@
+package com.enigma.wmbapi.constant;
+
+public enum UserRole {
+    ROLE_ADMIN, ROLE_CUSTOMER
+}
