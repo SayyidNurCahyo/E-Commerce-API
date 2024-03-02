03e4f4cd173a5f8688d5edad93db8f03e3efb155
Sayyid Nur Cahyo Abdul Jalil <sayyidnurc123@gmail.com>
2024-03-02 10:19:53 +0700

    [FEATURE] add repo and entity for user account and role

---
+++ src/main/java/com/enigma/wmbapi/entity/UserAccount.java
@@ -0,0 +1,70 @@
+package com.enigma.wmbapi.entity;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "m_user_account")
+public class UserAccount implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Column(name = "is_enable")
+    private Boolean isEnabled;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority(role.toString()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+}
---
+++ src/main/java/com/enigma/wmbapi/repository/UserAccountRepository.java
@@ -0,0 +1,9 @@
+package com.enigma.wmbapi.repository;
+
+import com.enigma.wmbapi.entity.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
+}
