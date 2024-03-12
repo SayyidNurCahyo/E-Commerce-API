b69f7885f8592b01555d33b6b8854a0f6c42cc84
Sayyid Nur Cahyo Abdul Jalil <sayyidnurc123@gmail.com>
2024-03-12 20:01:29 +0700

    fix commit

--- src/test/java/com/enigma/wmbapi/controller/ImageControllerTest.java
+++ src/test/java/com/enigma/wmbapi/controller/ImageControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
@@ -44,7 +45,7 @@ class ImageControllerTest {
     @Test
     void download() throws Exception {
         String id = "id";
-        Resource resource = new UrlResource(Paths.get("/Users/Lenovo/OneDrive/Gambar/WMB_API/1709990392567_bakwan3.jpg").toUri());
+        Resource resource = new UrlResource (Paths.get("/Users/Lenovo/OneDrive/Gambar/WMB_API/1709990392567_bakwan3.jpg").toUri());
         when(imageService.getById(anyString())).thenReturn(resource);
         String headerValue = "attachment; filename=1709990392567_bakwan3.jpg";
         mockMvc.perform(MockMvcRequestBuilders.get(APIUrl.IMAGE_DOWNLOAD_API+"/"+id)
